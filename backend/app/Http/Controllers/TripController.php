081286dda93633dcf2af4826f12bfe340fa75f11
dinabandhuk <dinabandhukhatiwada@gmail.com>
2025-02-25 14:26:05 +0545

    install laravel broadcasting package for websockets

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 
 use Illuminate\Http\Request;
@@ -53,6 +57,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripAccepted::dispatch($trip, $request->user());
         return $trip;
     }
 
@@ -64,6 +70,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripEnded::dispatch($trip, $request->user());
         return $trip;
     }
 
@@ -75,6 +83,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripStarted::dispatch($trip, $request->user());
         return $trip;
     }
 
@@ -90,6 +100,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip, $request->user());
         return $trip;
     }
 }
