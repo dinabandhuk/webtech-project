004a46b1c90023d33725188ff896db155c19d2b4
dinabandhuk <dinabandhukhatiwada@gmail.com>
2025-02-25 15:52:43 +0545

    add tailwindcss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,15 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="max-w-3xl mx-auto bg-green-100 text-center">
+      <h1>Hello, Tailwind</h1>
     </div>
   </header>
 
