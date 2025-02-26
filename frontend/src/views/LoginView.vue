eba8f236920ecd1dcba02582b954982e3ce1e029
dinabandhuk <dinabandhukhatiwada@gmail.com>
2025-02-26 15:46:15 +0545

    create login page with number validation
    axios sends the request to login route to
    laravel backend which in turn calls the twilio
    notifier to send the OTP.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="pt-16">
       <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-      <form @submit.prevent="handleSubmit">
+      <form action="#" @submit.prevent="handleLogin">
         <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
           <div class="bg-white px-4 py-5 sm:p-6">
             <div>
               <input
-                v-model="phone"
+                v-model="credentials.phone"
                 type="text"
+                v-maska data-maska="(###) ## ### #####"
                 name="phone"
                 id="phone"
-                placeholder="+977-1234567890"
+                placeholder="(977) 97 456 00000"
                 class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300"
               />
             </div>
           </div>
           <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <button
-              type="submit"
+            <button @click="submit" @submit.prevent="handleLogin"
               class="inline-flex justify-center rounded-md border border-transparent bg-black px-4 py-2 text-white"
             >
               Submit
@@ -29,6 +29,25 @@
   </template>
   
   <script setup>
- 
+import {vMaska} from 'maska/vue'
+import { reactive } from 'vue'
+import axios from 'axios'
+
+const credentials = reactive({
+  phone: null
+})
+
+const handleLogin = () =>{
+  axios.post('http://localhost:8000/api/login', {
+    phone : credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '')
+  })
+  .then((response) =>{
+    console.log(response.data)
+  })
+  .catch((error) =>{
+    console.error(error)
+    alert(error.response.data.message)
+  })
+}
   </script>
   
